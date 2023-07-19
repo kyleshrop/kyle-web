e20f7e04ea48f8b55433ff13105b9c7a878b7057
kyle <kyle@kyles-MacBook-Pro.local>
2023-07-19 17:44:48 -0600

    Revised HTML passing

--- src/index.js
+++ src/index.js
@@ -3,19 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send(`<!DOCTYPE html>
-  <html>
-  <head>
-  <title>Page Title</title>
-  </head>
-  <body>
-  
-  <h1>Kyle Shropshire's Sample Web Project</h1>
-  <p>Continue to Bio</p>
-  <a href="/bio">go to bio</a>
-  
-  </body>
-  </html>`)
+    res.sendFile(__dirname + "/pages/index.html")
 })
 
 app.get('/bio', (req, res) => {
