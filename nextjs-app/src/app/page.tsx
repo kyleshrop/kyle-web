29a4ec4151f1854fd958b148cae422e642588b2e
kyle <kyle@kyles-MacBook-Pro.local>
2023-07-20 16:14:13 -0600

    fixed lint error

--- nextjs-app/src/app/page.tsx
+++ nextjs-app/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main>
       <div>
-        <h1 className={styles.header}>Kyle Shropshire's Sample Web Project</h1>
+        <h1 className={styles.header}>Kyle Shropshires Sample Web Project</h1>
         <p className={styles.description}>Continue to Bio</p>
         <Image
           src="/windrunner.png"
