2e24a5e0287315566e8376bd041814027cbf084f
kyle <kyle@kyles-MacBook-Pro.local>
2023-07-20 15:28:33 -0600

    added formatting from reactive project

---
+++ nextjs-app/src/app/bio/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './bioRoute.module.css';
+
+export default function BioRoute() {
+  return (
+    <main>
+        <h1 className={styles.header}>Bio</h1>
+    </main>
+  );
+}
--- nextjs-app/src/app/page.tsx
+++ nextjs-app/src/app/page.tsx
@@ -1,16 +1,20 @@
 import Image from 'next/image'
-
+import styles from './indexRoute.module.css';
 
 export default function Home() {
   return (
     <main>
-         <Image
+      <div>
+        <h1 className={styles.header}>Kyle Shropshire's Sample Web Project</h1>
+        <p className={styles.description}>Continue to Bio</p>
+        <Image
           src="/windrunner.png"
           alt="Next.js Logo"
           width={180}
           height={37}
           priority
         />
+      </div>
     </main>
   )
 }
